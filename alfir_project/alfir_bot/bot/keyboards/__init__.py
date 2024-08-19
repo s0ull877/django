08c97b712750a3b4946ae95a8f777795ac6f245f
Radmir <galiullinradmir17@gmail.com>
2024-08-19 11:42:18 +0300

    update gitignore

--- alfir_project/alfir_bot/bot/keyboards/__init__.py
+++
@@ -1,3 +0,0 @@
-from .keyboards import share_contact, cancel_kb, without_password
-from .inline_keyboards import change_ikb, cancel_ikb, type_code, type_finish, \
-    type_finish
--- alfir_project/alfir_site/app/workers/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WorkersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workers'
-    verbose_name = 'Телеграм'
