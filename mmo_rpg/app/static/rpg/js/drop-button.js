912861ab245e3b37d17b85446aad42d8e8532fb7
Radja <galiullinradmir17@gmail.com>
2024-09-06 20:56:26 +0300

    0609

---
+++ mmo_rpg/app/static/rpg/js/drop-button.js
@@ -0,0 +1,17 @@
+function ShowRoles() {
+    document.getElementById("all-roles").classList.toggle("show");
+}
+
+function filterFunction() {
+    var filter, ul, li, a, i;
+    div = document.getElementsByClassName("all-roles")
+    console.log(div)
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
