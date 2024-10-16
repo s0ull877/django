e9d3baf00abea92d2757ef9b90085fb367ba3354
Radmir <galiullinradmir17@gmail.com>
2024-10-16 11:03:41 +0300

    RedirectModel create

--- redirect/app/redirect/models.py
+++ redirect/app/redirect/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class RecirectModel(models.Model):
+
+    path = models.CharField(
+        verbose_name='Путь редиректа',
+        unique=True
+    )
+    redirect_to = models.URLField(
+        verbose_name='Редирект на'
+    )
+
+    def __str__(self):
+        
+        return f'{self.id} | {self.path}'
