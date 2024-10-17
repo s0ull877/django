4142448a8b3ba5e5ad528ef2c33895e9aaa0e319
Radmir <galiullinradmir17@gmail.com>
2024-10-17 12:11:47 +0300

    rework path generation

--- redirect/app/redirect/serializers.py
+++ redirect/app/redirect/serializers.py
@@ -1,9 +1,13 @@
+import string
 from django.db.utils import IntegrityError
+from django.utils.crypto import get_random_string
+from django.conf import settings
 
 from rest_framework import serializers
-from .utils import generate_path
 
 from .models import RedirectModel
+
+settings.CHARACTERS = string.ascii_letters + string.digits
 
 class RedirectSerializer(serializers.ModelSerializer):
 
@@ -19,7 +23,7 @@ class RedirectSerializer(serializers.ModelSerializer):
         # if generate_path() get non unique data, i try to generate new path
         while True:
 
-            validated_data['path'] = generate_path()
+            validated_data['path'] = get_random_string(length=10, allowed_chars=settings.CHARACTERS)
 
             try:
                 instanse = ModelClass._default_manager.create(**validated_data)
